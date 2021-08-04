339a53175d36c50e28a21b9f74467c915d800d3b
Rachel Cleak <rachelmcleak@gmail.com>
2021-08-03 18:27:33 -0700

    Shark update

---
+++ static/js/sharkwords.js
@@ -0,0 +1,117 @@
+
+const ALPHABET = 'abcdefghijklmnopqrstuvwxyz';
+const WORDS = [
+  'strawberry',
+  'orange',
+  'apple',
+  'banana',
+  'pineapple',
+  'kiwi',
+  'peach',
+  'pecan',
+  'eggplant',
+  'durian',
+  'peanut',
+  'chocolate',
+];
+
+let numWrong = 0;
+
+// Loop over the chars in `word` and create divs.
+//
+const createDivsForChars = (word) => {
+  // loop through list
+  // generate divs for each letter
+  for (const letter of word){
+    $('#word-container').append(`<div class="letter-box ${letter}"></div>`);
+  }
+};
+
+// Loop over each letter in `ALPHABET` and generate buttons.
+// #letter-buttons
+// <button>a</button>
+const generateLetterButtons = () => {
+  for (const letter of ALPHABET) {
+    $('#letter-buttons').append(`<button>${letter}</button>`);
+  }
+};
+
+// Set the `disabled` property of `buttonEl` to `true.
+//
+// `buttonEl` is an `HTMLElement` object.
+// button.attr('src', 'cat2.gif');
+
+const disableLetterButton = (buttonEl) => {
+  // Replace this with your code
+  const button = $(buttonEl);
+  button.attr('disabled', 'true');
+  
+};
+
+// Return `true` if `letter` is in the word.
+//  $('#word-container').append(`<div class="letter-box ${letter}"></div>`);
+// check if $('div.${letter}') exists 
+const isLetterInWord = (letter) => {
+  let x = $(`div.${letter}`)[0];
+  if (x === undefined){
+    return false;
+  }
+  else {
+    return true;
+  }
+};
+
+// Called when `letter` is in word. Update contents of divs with `letter`.
+// $('#word-container')
+const handleCorrectGuess = (letter) => {
+  // Replace this with your code
+  $(`div.${letter}`).html(letter); 
+};
+
+// Called when `letter` is not in word.
+//
+// If the shark gets the person, disable all buttons and show the "play again"
+// message. Otherwise, increment `numWrong` and update the shark image.
+// numWrong=0 on top of page
+const handleWrongGuess = () => {
+  numWrong++;
+  if (numWrong <= 5) {
+    $('img').attr('src', `/static/images/guess${numWrong}.png`); 
+  } else {
+    $('#play-again').attr('style', '');
+  }
+};
+
+//  Reset game state. Called before restarting the game.
+//
+const resetGame = () => {
+  window.location = '/sharkwords';
+};
+
+// This is like if __name__ == '__main__' in Python
+//
+(function startGame() {
+  // For now, we'll hardcode the word that the user has to guess.
+  // let word = "hello";
+  let word = WORDS[Math.floor(Math.random()*WORDS.length)];
+
+  createDivsForChars(word);
+  generateLetterButtons();
+
+  $('button').on('click', (evt) => {
+    const clickedBtn = $(evt.target);
+    disableLetterButton(clickedBtn);
+
+    const letter = clickedBtn.html();
+
+    if (isLetterInWord(letter)) {
+      handleCorrectGuess(letter);
+    } else {
+      handleWrongGuess(letter);
+    }
+  });
+
+  $('#play-again').on('click', () => {
+    resetGame();
+  });
+})();
